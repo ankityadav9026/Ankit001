91fc5e2b7d4cebd9a45ec227c1a20c405d043273
Ankit01 <ankityadav8359994@gmail.com>
2026-09-12 18:46:24 +0530

    add content page

--- index.js
+++ index.js
@@ -23,9 +23,14 @@ app.get('/files/:filename',function(req,res){
       res.render("show", {filename:req.params.filename,  filedata:filedata});
     });
 })
-    // app.get('/edit/:filename',function(req,res){
-    //     res.render("edit",{filename:req.params.filename} );
-    // });
+    app.get('/edits/:filenames',function(req,res){
+        res.render("edits",{filenames:req.params.filenames} );
+    });
+    app.post('/edit',function(req,res){
+        fs.writeFile(`./files/${req.body.previouss}`,`./files/${req.body.news}`,function(err){
+            res.redirect("/");
+        })
+     })
  app.post('/edit',function(req,res){
     fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new}`,function(err){
         res.redirect("/");
@@ -38,6 +43,6 @@ res.redirect("/")
 });
 
 
- 
+
 });
 app.listen(3000);   
